177b70697ebf8d8a9ba03af68e83eafc9b1b4da9
bluerama <ramanana@buffalo.edu>
2017-02-26 21:31:16 +0530

    sketch.js

--- sketch.js
+++ sketch.js
@@ -3,5 +3,9 @@ function setup() {
 }
 
 function draw() {
-        text("Ramana Nagasamudram", mouseX, mouseY);
+        background(200);
+        fill(255);
+        textSize(40);
+        textLeading((mouseX / width) * 80);
+        text("Site under construction", 100, 100, 200, 200);
 }
