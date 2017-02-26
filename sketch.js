0ada104cc9379ed623ea1bfaedeb4e6b86346f51
bluerama <ramanana@buffalo.edu>
2017-02-26 21:37:09 +0530

    Initial commit

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
+var phrase = "Under construction...";
 function setup() {
         createCanvas(640, 480);
+        frameRate(10);
 }
 
 function draw() {
         background(200);
         fill(255);
         textSize(40);
-        textLeading((mouseX / width) * 80);
-        text("Site under construction", 100, 100, 200, 200);
+        textAlign(CENTER, CENTER);
+        var middle = phrase.length / 2;
+        var left = middle - ((mouseX / width) * middle);
+        var right = middle - ((mouseX / width) * middle);
+        text(phrase.substring(left, right+1), width/2, height/2);
 }
